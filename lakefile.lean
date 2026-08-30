bbf4ec079f5d22ef6ffb8f2011e6fca752f4ab1e
Emilio Jesus Gallego Arias <emilio@lean-fro.org>
2026-08-30 22:33:57 +0200

    refactor: migrate informal block storage to modules

--- lakefile.lean
+++ lakefile.lean
@@ -109,6 +109,7 @@ lean_lib VersoBlueprintModuleTests where
   srcDir := "tests"
   roots := #[
     `VersoBlueprintModuleTests.Attribute,
+    `VersoBlueprintModuleTests.BlockStore,
     `VersoBlueprintModuleTests.Data,
     `VersoBlueprintModuleTests.DependencyAnalysis,
     `VersoBlueprintModuleTests.ExternalDeclRender,
