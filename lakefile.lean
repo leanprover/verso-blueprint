f757ce32e28bec34695dfa2268d885f3ba136a6f
Emilio Jesus Gallego Arias <emilio@lean-fro.org>
2026-08-30 22:36:23 +0200

    refactor: migrate hover rendering to modules

--- lakefile.lean
+++ lakefile.lean
@@ -115,6 +115,7 @@ lean_lib VersoBlueprintModuleTests where
     `VersoBlueprintModuleTests.ExternalRefSnapshot,
     `VersoBlueprintModuleTests.Foundation,
     `VersoBlueprintModuleTests.Graph,
+    `VersoBlueprintModuleTests.HoverRender,
     `VersoBlueprintModuleTests.Math,
     `VersoBlueprintModuleTests.MathLeaves,
     `VersoBlueprintModuleTests.RuntimeServices,
