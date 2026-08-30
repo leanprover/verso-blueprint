13e201809799cfd0c768893f7a8949a5da7eb758
Emilio Jesus Gallego Arias <emilio@lean-fro.org>
2026-08-31 00:31:30 +0200

    refactor: migrate summary collection to modules

--- lakefile.lean
+++ lakefile.lean
@@ -137,6 +137,7 @@ lean_lib VersoBlueprintModuleTests where
     `VersoBlueprintModuleTests.SourceAuthoring,
     `VersoBlueprintModuleTests.SourceData,
     `VersoBlueprintModuleTests.SourceMetadata,
+    `VersoBlueprintModuleTests.SummaryCollect,
     `VersoBlueprintModuleTests.SummaryData,
     `VersoBlueprintModuleTests.TeXRuntime,
     `VersoBlueprintModuleTests.TraversalIndex,
