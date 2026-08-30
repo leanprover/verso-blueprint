362ba689a8d4501ef30c178e36de5619dcd34831
Emilio Jesus Gallego Arias <emilio@lean-fro.org>
2026-08-30 23:37:41 +0200

    refactor: migrate informal block authoring to modules

--- lakefile.lean
+++ lakefile.lean
@@ -109,6 +109,7 @@ lean_lib VersoBlueprintModuleTests where
   srcDir := "tests"
   roots := #[
     `VersoBlueprintModuleTests.Attribute,
+    `VersoBlueprintModuleTests.BlockAuthoring,
     `VersoBlueprintModuleTests.BlockCommon,
     `VersoBlueprintModuleTests.BlockStore,
     `VersoBlueprintModuleTests.Data,
