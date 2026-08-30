09cad2c1de3254655b8e81ae646bcebb6e93a2a4
Emilio Jesus Gallego Arias <emilio@lean-fro.org>
2026-08-31 00:56:51 +0200

    refactor: migrate bibliography authoring to modules

--- lakefile.lean
+++ lakefile.lean
@@ -108,6 +108,7 @@ lean_lib VersoBlueprintModuleTests where
   roots := #[
     `VersoBlueprintModuleTests.Attribute,
     `VersoBlueprintModuleTests.AuthorAuthoring,
+    `VersoBlueprintModuleTests.BibliographyAuthoring,
     `VersoBlueprintModuleTests.BlockAuthoring,
     `VersoBlueprintModuleTests.BlockCommon,
     `VersoBlueprintModuleTests.BlockStore,
