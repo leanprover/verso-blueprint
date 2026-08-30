7897fb062fed4032e91ebe248486751ca1261494
Emilio Jesus Gallego Arias <emilio@lean-fro.org>
2026-08-30 22:28:31 +0200

    refactor: migrate traversal index to modules

--- lakefile.lean
+++ lakefile.lean
@@ -118,6 +118,7 @@ lean_lib VersoBlueprintModuleTests where
     `VersoBlueprintModuleTests.MathLeaves,
     `VersoBlueprintModuleTests.RuntimeServices,
     `VersoBlueprintModuleTests.SourceData,
+    `VersoBlueprintModuleTests.TraversalIndex,
     `VersoBlueprintModuleTests.UtilityLeaves
   ]
   requiresModuleSystem := true
