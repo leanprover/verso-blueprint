466bb92ed55aef0dfdecb9befcf4b53a37bb9201
Emilio Jesus Gallego Arias <emilio@lean-fro.org>
2026-08-31 00:12:21 +0200

    refactor: migrate serialized command blocks to modules

--- lakefile.lean
+++ lakefile.lean
@@ -128,6 +128,7 @@ lean_lib VersoBlueprintModuleTests where
     `VersoBlueprintModuleTests.MathLeaves,
     `VersoBlueprintModuleTests.RuntimeServices,
     `VersoBlueprintModuleTests.RustAuthoring,
+    `VersoBlueprintModuleTests.SerializedExtension,
     `VersoBlueprintModuleTests.SourceAuthoring,
     `VersoBlueprintModuleTests.SourceData,
     `VersoBlueprintModuleTests.SourceMetadata,
