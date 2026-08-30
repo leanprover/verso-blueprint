ea5b7af0d2120fe78f36650facfbef27f033c50e
Emilio Jesus Gallego Arias <emilio@lean-fro.org>
2026-08-30 18:43:07 +0200

    test: establish module-system refactor baseline

--- lakefile.lean
+++ lakefile.lean
@@ -86,6 +86,7 @@ lean_lib VersoBlueprintTests where
     `VersoBlueprintTests.BlueprintPreviewSchema,
     `VersoBlueprintTests.BlueprintPreviewSource,
     `VersoBlueprintTests.BlueprintPreviewWiring,
+    `VersoBlueprintTests.BlueprintPublicRoot,
     `VersoBlueprintTests.BlueprintSource,
     `VersoBlueprintTests.BlueprintSourceIdentity,
     `VersoBlueprintTests.BlueprintRustCode,
