a0db7e278b15f5d5ad798cc22346ba8db5b25d11
Emilio Jesus Gallego Arias <emilio@lean-fro.org>
2026-08-31 00:41:14 +0200

    refactor: migrate summary authoring to modules

---
+++ tests/VersoBlueprintModuleTests/SummaryAuthoring.lean
@@ -0,0 +1,21 @@
+/-
+Copyright (c) 2026 Lean FRO LLC. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author: Emilio J. Gallego Arias
+-/
+
+module
+
+import VersoBlueprint.Commands.Summary
+meta import VersoBlueprint.Commands.Summary
+
+namespace VersoBlueprintModuleTests.SummaryAuthoring
+
+open Verso Genre Manual
+
+#docs (Manual) summaryAuthoringContractDoc "Module Summary Authoring" :=
+:::::::
+{blueprint_summary}
+:::::::
+
+end VersoBlueprintModuleTests.SummaryAuthoring
