a860f593d6e36344e8b3db564d01c92bef57256f
Emilio Jesus Gallego Arias <emilio@lean-fro.org>
2026-08-30 23:48:30 +0200

    refactor: migrate informal code authoring to modules

--- lakefile.lean
+++ lakefile.lean
@@ -112,6 +112,7 @@ lean_lib VersoBlueprintModuleTests where
     `VersoBlueprintModuleTests.BlockAuthoring,
     `VersoBlueprintModuleTests.BlockCommon,
     `VersoBlueprintModuleTests.BlockStore,
+    `VersoBlueprintModuleTests.CodeAuthoring,
     `VersoBlueprintModuleTests.Data,
     `VersoBlueprintModuleTests.DependencyAnalysis,
     `VersoBlueprintModuleTests.ExternalDeclRender,
