7bb1e6484e06b83213ff6698c64f870a114d3f6c
Emilio Jesus Gallego Arias <emilio@lean-fro.org>
2026-08-30 22:39:19 +0200

    refactor: migrate group authoring to modules

--- lakefile.lean
+++ lakefile.lean
@@ -98,6 +98,7 @@ lean_lib VersoBlueprintModuleTests where
     `VersoBlueprintModuleTests.ExternalRefSnapshot,
     `VersoBlueprintModuleTests.Foundation,
     `VersoBlueprintModuleTests.Graph,
+    `VersoBlueprintModuleTests.GroupAuthoring,
     `VersoBlueprintModuleTests.HoverRender,
     `VersoBlueprintModuleTests.Math,
     `VersoBlueprintModuleTests.MathLeaves,
