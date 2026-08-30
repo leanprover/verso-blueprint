59b46feaca31852129bcb2ed89fc9c7f5a38e6bd
Emilio Jesus Gallego Arias <emilio@lean-fro.org>
2026-08-30 22:47:03 +0200

    refactor: migrate external markup views to modules

--- lakefile.lean
+++ lakefile.lean
@@ -114,6 +114,7 @@ lean_lib VersoBlueprintModuleTests where
     `VersoBlueprintModuleTests.ExternalDeclRender,
     `VersoBlueprintModuleTests.ExternalDeclRenderData,
     `VersoBlueprintModuleTests.ExternalRefSnapshot,
+    `VersoBlueprintModuleTests.ExternalMarkupView,
     `VersoBlueprintModuleTests.Foundation,
     `VersoBlueprintModuleTests.Graph,
     `VersoBlueprintModuleTests.GroupAuthoring,
--- src/VersoBlueprint/Informal/ExternalMarkupView.lean
+++ src/VersoBlueprint/Informal/ExternalMarkupView.lean
@@ -4,8 +4,12 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author: Emilio J. Gallego Arias
 -/
 
-import VersoBlueprint.Data
-import VersoBlueprint.Html
+module
+
+public import VersoBlueprint.Data
+public import VersoBlueprint.Html
+
+public section
 
 namespace Informal.ExternalMarkupView
 
