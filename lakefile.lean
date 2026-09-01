d422fe331d8912248326cfafd23bd897429c5030
Emilio Jesus Gallego Arias <emilio@lean-fro.org>
2026-09-01 18:15:04 +0200

    refactor: migrate graft authoring to modules

--- lakefile.lean
+++ lakefile.lean
@@ -129,6 +129,7 @@ lean_lib VersoBlueprintModuleTests where
     `VersoBlueprintModuleTests.GraphAuthoring,
     `VersoBlueprintModuleTests.GraphData,
     `VersoBlueprintModuleTests.GraftAssets,
+    `VersoBlueprintModuleTests.GraftAuthoring,
     `VersoBlueprintModuleTests.GraftNode,
     `VersoBlueprintModuleTests.GraftRender,
     `VersoBlueprintModuleTests.GroupAuthoring,
