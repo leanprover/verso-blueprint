853b893598415c230d4f6f0c55ec51e66cede8d1
Emilio Jesus Gallego Arias <emilio@lean-fro.org>
2026-09-01 18:11:45 +0200

    refactor: migrate Slides integration to modules

--- lakefile.lean
+++ lakefile.lean
@@ -145,6 +145,7 @@ lean_lib VersoBlueprintModuleTests where
     `VersoBlueprintModuleTests.RuntimeServices,
     `VersoBlueprintModuleTests.RustAuthoring,
     `VersoBlueprintModuleTests.SerializedExtension,
+    `VersoBlueprintModuleTests.Slides,
     `VersoBlueprintModuleTests.SlidesAssets,
     `VersoBlueprintModuleTests.SlidesNode,
     `VersoBlueprintModuleTests.SlidesRender,
