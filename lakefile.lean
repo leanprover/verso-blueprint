e4102c54963bed54653811e8ad90f57c0c1e9df2
Emilio Jesus Gallego Arias <emilio@lean-fro.org>
2026-09-01 18:07:41 +0200

    refactor: migrate slide node authoring to modules

--- lakefile.lean
+++ lakefile.lean
@@ -126,6 +126,7 @@ lean_lib VersoBlueprintModuleTests where
     `VersoBlueprintModuleTests.RuntimeServices,
     `VersoBlueprintModuleTests.RustAuthoring,
     `VersoBlueprintModuleTests.SerializedExtension,
+    `VersoBlueprintModuleTests.SlidesNode,
     `VersoBlueprintModuleTests.SourceAuthoring,
     `VersoBlueprintModuleTests.SourceData,
     `VersoBlueprintModuleTests.SourceMetadata,
